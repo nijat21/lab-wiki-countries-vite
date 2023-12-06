a0b0372f9fcfe2c6c20e57bd13eae6207d061ec8
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2023-12-06 21:59:44 +0000

    last two iterations left

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,52 @@
-function CountryDetails() {}
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function CountryDetails() {
+    const { countryId } = useParams();
+    const [countryFound, setCountryFound] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then((response) => {
+                console.log(response.data);
+                setCountryFound(response.data);
+            })
+            .catch((err) => console.log(err));
+    }, [countryId])
+
+    return (
+        <>
+            <div className="container">
+                <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+                {countryFound && (
+                    <>
+                        <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryFound.alpha2Code.toLowerCase()}.png`} alt="" />
+                        <h1>{countryFound.name.common}</h1>
+                        <table className="table">
+                            <thead>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style={{ width: "30%" }}>Capital</td>
+                                    <td>{countryFound.capital}</td>
+                                </tr>
+                                <tr>
+                                    <td>Area</td>
+                                    <td>{countryFound.area}</td>
+                                </tr>
+                                <tr>
+                                    <td>Borders</td>
+                                    <td>{countryFound.borders}</td>
+                                </tr>
+                            </tbody>
+                            <tfoot></tfoot>
+                        </table>
+                    </>
+                )}
+            </div>
+        </>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,41 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function HomePage() {
+    const [countries, setCountries] = useState(null);
+
+    useEffect(() => {
+        axios.get("https://ih-countries-api.herokuapp.com/countries")
+            .then((response) => {
+                console.log(response.data);
+                setCountries(response.data);
+            })
+            .catch((err) => console.log(err));
+    }, [])
+
+
+    return (
+        <div className="container" style={{ maxHeight: "90vh", overflow: scroll }}>
+            <h1 style={{ fontSize: "24px" }}>Your Guide to the World</h1>
+
+            <div className="listGroup">
+                {countries &&
+                    (countries.map((country) => {
+                        return (
+                            <>
+                                <Link to={`/${country.alpha3Code}`} className="list-group-item list-group-item-action">
+                                    <h3>
+                                        <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} />
+                                        {country.name.common}</h3>
+                                </Link>
+                            </>
+                        )
+                    }))
+                }
+            </div>
+        </div>
+    )
+}
 
 export default HomePage;
